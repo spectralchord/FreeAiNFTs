9581348bbd938bd7ea40dce92936f486e5f014b5
spectralchord <100803793+spectralchord@users.noreply.github.com>
2023-10-05 21:43:09 +0300

    update register.ts

--- frontend-app/src/share/register.ts
+++ frontend-app/src/share/register.ts
@@ -1,13 +1,13 @@
 import {Contract, ethers} from 'ethers';
 import {signMetaTxRequest} from './signer';
 import {apiRegister} from "@/apis";
-import {ForwarderAddress, SubscriptionContractAddress} from "@/share/ethersConfig";
+import {ForwarderAddress} from "@/share/ethersConfig";
 import {ForwarderAbi, SubscriptionContractAbi} from "@/share/ethersABIs";
 
-export const sendMetaTx = async (signer: any, preference: string) => {
+export const sendMetaTx = async (signer: any, preference: string, contractAddress: string) => {
     const from = await signer.getAddress();
     const forwarder = new Contract(ForwarderAddress, ForwarderAbi, signer);
-    const notSubscription = new Contract(SubscriptionContractAddress[0], SubscriptionContractAbi, signer);
+    const notSubscription = new Contract(contractAddress, SubscriptionContractAbi, signer);
     const data = ethers.utils.defaultAbiCoder.encode(["string"], [preference]);
     const aftSubscriptionAddr = notSubscription.address;
     const request = await signMetaTxRequest(signer.provider, forwarder, {
@@ -18,10 +18,11 @@ export const sendMetaTx = async (signer: any, preference: string) => {
     await apiRegister(request)
 };
 
-export const registerName = async ({preference}: { preference: string }) => {
-    if (!window.ethereum) throw new Error(`User wallet not found`);
+export const registerName = async ({preference, contractAddress}: { preference: string, contractAddress: string | undefined }) => {
+    if (!window.ethereum) throw new Error(User wallet not found);
     await window.ethereum.enable();
     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
     const signer = await provider.getSigner();
-    return sendMetaTx(signer, preference);
+    if (contractAddress)
+        return sendMetaTx(signer, preference, contractAddress);
 };
